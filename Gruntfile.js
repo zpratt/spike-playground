8e28383f707662cbdd053141c501d6a5b3448618
zach pratt <zach@attackofzach.com>
2014-08-08 00:12:48 -0500

    added grunt task to concat dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,39 @@
 module.exports = function(grunt) {
 
+    var _ = require('lodash'),
+
+        thirdPartyDependencies = [
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/lodash/dist/lodash.compat.min.js',
+            'bower_components/backbone/backbone.js',
+            'bower_components/react/react.min.js',
+            'bower_components/d3/d3.min.js'
+        ],
+        sharedProdDependencies = [
+            'js/namespace.js',
+            'js/map.js',
+            'js/iowa.js',
+
+            'js/components/answers-model.js',
+            'js/components/answer-collection.js'
+        ],
+        reactExample = [
+            'js/view.js'
+        ],
+        boundaryExample = [
+            'js/donut-graph.js',
+            'js/ground-view.js',
+            'js/ground-view-example.js'
+        ],
+        quadtreeExample = [
+            'js/quadtree-example.js'
+        ];
+
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-react');
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Project configuration.
     grunt.initConfig({
@@ -26,12 +57,31 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 files: {
-                    'css/main.css': 'sass/*.scss'
+                    'dist/css/main.css': 'sass/*.scss'
                 }
             }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            thirdparty: {
+                src: thirdPartyDependencies,
+                dest: 'dist/js/deps.js'
+            },
+            quadtreeExample: {
+                options: {
+                    sourceMap: true
+                },
+                src: _.union(sharedProdDependencies, quadtreeExample),
+                dest: 'dist/js/quadtree.js'
+            }
+        },
+        clean: {
+            build: ['dist']
         }
     });
 
     // Default task(s).
-    grunt.registerTask('default', ['react','sass']);
+    grunt.registerTask('default', ['clean', 'react', 'sass', 'concat']);
 };
